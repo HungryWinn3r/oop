a32a1584e011a436672af0e46da6ab265c4ff78d
L M <greezle.gg@mail.ru>
2022-01-06 18:56:59 +0300

    lab-4

--- Isu/Tools/IsuException.cs
+++ Isu/Tools/IsuException.cs
@@ -1,19 +1,3 @@
-﻿using System;
-
-namespace Isu.Tools
+﻿namespace Isu.Tools
 {
-    public class IsuException : Exception
-    {
-        public IsuException()
-        {
-        }
-
-        public IsuException(string message) : base(message)
-        {
-        }
-
-        public IsuException(string message, Exception innerException) : base(message, innerException)
-        {
-        }
-    }
 }
