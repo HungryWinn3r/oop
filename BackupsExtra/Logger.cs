b430bb4609001b3b5df9dd85b3c0d7c0bf33ca76
Лев Сергеевич Максимов <311364@niuitmo.ru>
2022-01-17 13:27:37 +0300

    lab-5

---
+++ BackupsExtra/Logger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace BackupsExtra
+{
+    public class Logger
+    {
+        private FileStream _fileStream;
+        public Logger(string filename)
+        {
+            _fileStream = new FileStream(filename, FileMode.Append);
+        }
+
+        public bool Timecode { get; set; }
+        public void Write(string message)
+        {
+            if (Timecode)
+                _fileStream.Write(System.Text.Encoding.Default.GetBytes(DateTime.Now + ": " + message));
+            _fileStream.Write(System.Text.Encoding.Default.GetBytes(message));
+        }
+    }
+}
---
+++ BackupsExtra/Merge.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Backups;
+
+namespace BackupsExtra.DeleterAlgorithm
+{
+    public class Merge
+    {
+        public void Run(List<RestorePoint> restorePoints, List<RestorePoint> restorePointsToRemove, string storage)
+        {
+            string startDir = Directory.GetCurrentDirectory();
+            Directory.SetCurrentDirectory(storage);
+            RestorePoint newRestorePoint = restorePoints.Except(restorePointsToRemove).First();
+            foreach (RestorePoint oldRestorePoint in restorePointsToRemove)
+            {
+                foreach (string jobObject in oldRestorePoint.Files)
+                {
+                    if (newRestorePoint.Files.Exists(jobObject1 => jobObject1 == jobObject)) continue;
+                    File.Copy($"./{oldRestorePoint.Name}/{jobObject}", $"./{newRestorePoint.Name}/{jobObject}");
+                    newRestorePoint.Files.Add(jobObject);
+                }
+
+                Directory.Delete($"./{oldRestorePoint.Name}", true);
+                restorePoints.Remove(oldRestorePoint);
+            }
+
+            Directory.SetCurrentDirectory(startDir);
+        }
+    }
+}
